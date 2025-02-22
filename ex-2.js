05f6c2521a36b1091ef74564c8dee97c5c862a24
Tae <taewang48@gmail.com>
2025-02-23 00:26:43 +0700

    HW:Logic

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,13 @@
 //Exercise 2
 let lightBulbStatus = "On";
+lightBulbStatus = "Broken";
+let show;
+ if (lightBulbStatus === "On"){
+    show = "Light bulb is On."
+ } else if (lightBulbStatus === "Off") {
+    show = "Light bulb is Off."
+ } else if (lightBulbStatus === "Broken") {
+    show = "Light bulb is Broken."
+ } else {show = "Please choose the correct input (On/Off/Broken)"}
 
-// Start coding here.
+ console.log(show)
--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,13 @@
 let lightBulbStatus = "Off";
 
 //Start coding here
+let show;
+    lightBulbStatus === "On"
+    ? (show = "Light bulb is On.")
+    : lightBulbStatus === "Off"
+    ? (show = "Light bulb is Off.")
+    : lightBulbStatus === "Broken"
+    ? (show = "Light bulb is Broken.")
+    : (show = "Please choose the correct input (On/Off/Broken)")
+
+console.log(show)
